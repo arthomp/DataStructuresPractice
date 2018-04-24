4266e946eb2dbe7b0f3a18354ee2be28c2b3978f
Austin <athomp12@binghamton.edu>
2018-04-24 02:28:08 -0400

    added implementation of pushFront, pushBack, popFront, popBack, and printList

--- Doubly_Linked_List/DoublyLinkedList.h
+++ Doubly_Linked_List/DoublyLinkedList.h
@@ -1,6 +1,8 @@
 #ifndef DOUBLYLINKEDLIST_H
 #define DOUBLYLINKEDLIST_H
 
+#include <iostream>
+
 template<class T>
 class DoublyLinkedList {
 	private:
@@ -9,17 +11,23 @@ class DoublyLinkedList {
 			Node *next, *prev;
 		};
 		Node *head, *tail;
+		int length;
 	public:
 		DoublyLinkedList();
 		~DoublyLinkedList();
 		bool isEmpty();
+		void printList();
 		void pushFront(T val);
+		void pushBack(T val);
+		void popFront();
+		void popBack();
 };
 
 template<class T>
 DoublyLinkedList<T>::DoublyLinkedList() {
 	head = nullptr;
-	tail = nullptr;	
+	tail = nullptr;
+	length = 0;
 }
 
 template<class T>
@@ -36,12 +44,100 @@ DoublyLinkedList<T>::~DoublyLinkedList() {
 
 template<class T>
 bool DoublyLinkedList<T>::isEmpty() {
-	return (head == nullptr && tail == nullptr);
+	return (length == 0);
 }
 
 template<class T>
 void DoublyLinkedList<T>::pushFront(T val) {
+	Node *temp = new Node();
+	temp->data = val;
+	if(isEmpty()) {
+		temp->next = nullptr;
+		temp->prev = nullptr;
+		head = temp;
+		tail = temp;
+	} else {
+		temp->next = head;
+		head->prev = temp;
+		temp->prev = nullptr;
+		head = temp;	
+	}
+	length++;
+}
 
+template<class T>
+void DoublyLinkedList<T>::pushBack(T val) {
+	Node *temp = new Node();
+	temp->data = val;
+	if(isEmpty()) {
+		temp->next = nullptr;
+		temp->prev = nullptr;
+		head = temp;
+		tail = temp;
+	} else {
+		temp->prev = tail;
+		tail->next = temp;
+		temp->next = nullptr;
+		tail = temp;
+	}
+	length++;
+}
+
+template<class T>
+void DoublyLinkedList<T>::popFront() {
+	if(isEmpty()) {
+		std::cout << "Err(popFront): Can't delete from empty list!" << std::endl;
+	} else {
+		Node *temp;
+		temp = head;
+		head = head->next;
+		delete temp;
+		length--;
+		if(head != nullptr) {
+			head->prev = nullptr;
+		} else {
+			tail = head;
+		}
+	}
+}
+
+template<class T>
+void DoublyLinkedList<T>::popBack() {
+	if(isEmpty()) {
+		std::cout << "Err(popBack): Can't delete from empty list!" << std::endl;
+	} else {
+		Node *temp;
+		temp = tail;
+		tail = tail->prev;
+		delete temp;
+		length--;
+		if(tail != nullptr) {
+			tail->next = nullptr;
+		} else {
+			head = tail;
+		}
+	}
+}
+
+template<class T>
+void DoublyLinkedList<T>::printList() {
+	std::cout << "---------------------" << std::endl;
+	if(isEmpty()) {
+		std::cout << "List is empty!" << std::endl;
+	} else {
+		Node *temp;
+		temp = head;
+		std::cout << "Length of list: " << length << std::endl << "List = ";
+		while(temp != nullptr) {
+			std::cout << "[" << temp->data << "]";
+			if(temp->next != nullptr) {
+				std::cout << "-->";
+			}
+			temp = temp->next;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
 }
 
 #endif /* DOUBLYLINKEDLIST_H */
--- Doubly_Linked_List/Driver.cpp
+++ Doubly_Linked_List/Driver.cpp
@@ -1,10 +1,42 @@
 #include "DoublyLinkedList.h"
 #include <iostream>
-#include <typeinfo>
 
 int main() {
 	DoublyLinkedList<int> dll;
-	std::cout << "List is empty: " << dll.isEmpty() << std::endl;
-	std::cout << "Type is " << typeid(dll).name() << std::endl;
+	std::cout << "Checking if list is empty: " << dll.isEmpty() << std::endl;
+	dll.printList();
+
+	std::cout << "Inserting node to empty list (pushFront)..." << std::endl;
+	dll.pushFront(1);
+
+	std::cout << "Checking if list is empty: " << dll.isEmpty() << std::endl;
+	dll.printList();
+
+	std::cout << "Inserting second node to list (pushFront)..." << std::endl;
+	dll.pushFront(2);
+
+	std::cout << "Checking if list is empty: " << dll.isEmpty() << std::endl;
+	dll.printList();
+
+	std::cout << "Inserting third node to list (pushBack)..." << std::endl;
+	dll.pushBack(3);
+	dll.printList();
+
+	std::cout << "Deleting head node (popBack)..." << std::endl;
+	dll.popBack();
+	dll.printList();
+
+	std::cout << "Deleting head node (popFront)..." << std::endl;
+	dll.popFront();
+	dll.printList();
+
+	std::cout << "Deleting head node (popFront)..." << std::endl;
+	dll.popFront();
+	dll.printList();
+
+	std::cout << "Deleting head node (popFront)..." << std::endl;
+	dll.popFront();
+	dll.printList();
+
 	return 0;
 }
