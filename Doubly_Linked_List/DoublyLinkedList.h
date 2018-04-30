3401f33bdcfa15200cfd71c95f327a749ee8b669
Austin <athomp12@binghamton.edu>
2018-04-29 20:40:17 -0400

    sentinel nodes, add/alter functions, add comments
    -change implementation of list to use front and back sentinel nodes
    -add new list functions: find(), front(), back()
    -add new iterator functions: next(), prev()
    -alter existing functions to incorprate new sentinel nodes
    -add comments to define behavior and expectations

--- Doubly_Linked_List/DoublyLinkedList.h
+++ Doubly_Linked_List/DoublyLinkedList.h
@@ -1,3 +1,10 @@
+/**
+ * DoublyLinkedList.h
+ * Purpose: Custom implementation of templated doubly linked list with iterators
+ *
+ * @author: Austin Thompson
+*/
+
 #ifndef DOUBLYLINKEDLIST_H
 #define DOUBLYLINKEDLIST_H
 
@@ -5,8 +12,8 @@
 
 template<typename T>
 class DoublyLinkedList {
-	class Node;
 	private:
+		class Node;
 		Node *head, *tail;
 		int length;
 	public:
@@ -15,14 +22,17 @@ class DoublyLinkedList {
 		~DoublyLinkedList();
 		bool isEmpty() const;
 		int getLength() const;
-		void printList() const;
+		void printList();
 		void pushFront(const T val);
 		void pushBack(const T val);
 		void popFront();
 		void popBack();
 		void reverse();
+		Iterator find(const T val);
 		Iterator begin();
 		Iterator end();
+		T front();
+		T back();
 };
 
 template<typename T>
@@ -36,7 +46,6 @@ template<typename T>
 class DoublyLinkedList<T>::Iterator {
 	private:
 		Node *current;
-
 	public:
 		Iterator(Node *n);
 		Iterator& operator++();
@@ -46,19 +55,39 @@ class DoublyLinkedList<T>::Iterator {
 		T& operator*() const;
 		bool operator==(const Iterator& other) const;
 		bool operator!=(const Iterator& other) const;
+		Iterator next(int n = 1);
+		Iterator prev(int n = 1);
 };
 
+/**
+ * Constructor: Iterator
+ * Description: creates bidirectional iterator object that points to node in list
+ * Parameters: Node *n
+ * Return: none
+*/
 template<typename T>
 DoublyLinkedList<T>::Iterator::Iterator(Node *n) {
 	current = n;
 }
 
+/**
+ * Overload: prefix ++ operator
+ * Description: defines prefix ++ behavior for iterator
+ * Parameters: none
+ * Return: Iterator&
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator& DoublyLinkedList<T>::Iterator::operator++() {
 	current = current->next;
 	return *this;
 }
 
+/**
+ * Overload: postfix ++ operator
+ * Description: defines postfix ++ behavior for iterator
+ * Parameters: int (dummy parameter)
+ * Return: Iterator
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::operator++(int) {
 	Iterator temp = *this;
@@ -66,12 +95,24 @@ typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::operator++
 	return temp;
 }
 
+/**
+ * Overload: prefix -- operator
+ * Description: defines prefix -- behavior for iterator
+ * Parameters: none
+ * Return: Iterator&
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator& DoublyLinkedList<T>::Iterator::operator--() {
 	current = current->prev;
 	return *this;
 }
 
+/**
+ * Overload: postfix -- operator
+ * Description: defines postfix -- behavior for iterator
+ * Parameters: int (dummy parameter)
+ * Return: Iterator
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::operator--(int) {
 	Iterator temp = *this;
@@ -79,169 +120,306 @@ typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::operator--
 	return temp;
 }
 
+/**
+ * Overload: dereference operator (*)
+ * Description: defines dereference behavior for iterator
+ * Parameters: none
+ * Return: T&
+*/
 template<typename T>
 T& DoublyLinkedList<T>::Iterator::operator*() const {
 	return current->data;
 }
 
+/**
+ * Overload: comparison operator (==)
+ * Description: defines comparison behavior for iterator
+ * Parameters: const Iterator& other
+ * Return: bool
+*/
 template<typename T>
 bool DoublyLinkedList<T>::Iterator::operator==(const Iterator& other) const {
 	return current == other.current;
 }
 
+/**
+ * Overload: comparison operator (!=)
+ * Description: defines comparison behavior for iterator
+ * Parameters: const Iterator& other
+ * Return: bool
+*/
 template<typename T>
 bool DoublyLinkedList<T>::Iterator::operator!=(const Iterator& other) const {
 	return current != other.current;
 }
 
+/**
+ * Function: next
+ * Parameters: n (default = 1)
+ * Description: provides iterator pointing to the node it would be pointing to
+ *				if advanced n positions
+ * Return: Iterator
+*/
+template<typename T>
+typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::next(int n) {
+	Iterator itr = *this;
+	for(int i = 0; i < n; ++i) {
+		++itr;
+	}
+	return itr;
+}
+
+/**
+ * Function: prev
+ * Parameters: n (default = 1)
+ * Description: provides iterator pointing to the node it would be pointing to
+ *				if advanced -n positions
+ * Return: Iterator
+*/
+template<typename T>
+typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::Iterator::prev(int n) {
+	Iterator itr = *this;
+	for(int i = 0; i < n; ++i) {
+		--itr;
+	}
+	return itr;
+}
+
+/**
+ * Constructor: DoublyLinkedList
+ * Description: creates list object by initializing head and tail sentinel nodes
+ * Parameters: none
+ * Return: none
+*/
 template<typename T>
 DoublyLinkedList<T>::DoublyLinkedList() {
-	head = nullptr;
-	tail = nullptr;
+	head = new Node();
+	tail = new Node();
+	head->prev = nullptr;
+	head->next = tail;
+	tail->prev = head;
+	tail->next = nullptr;
 	length = 0;
 }
 
+/**
+ * Destructor: ~DoublyLinkedList
+ * Description: removes all nodes from list by calling popFront until empty, then
+ *              deletes head and tail
+ * Parameters: none
+ * Return: none
+*/
 template<typename T>
 DoublyLinkedList<T>::~DoublyLinkedList() {
 	while(!isEmpty()) {
 		popFront();
 	}
+	delete head;
+	delete tail;
 }
 
+/**
+ * Function: isEmpty
+ * Description: checks if length of list is equal to 0
+ * Parameters: none
+ * Return: bool
+*/
 template<typename T>
 bool DoublyLinkedList<T>::isEmpty() const {
 	return (length == 0);
 }
 
+/**
+ * Function: getLength
+ * Description: gets length of list
+ * Parameters: none
+ * Return: int
+*/
 template<typename T>
 int DoublyLinkedList<T>::getLength() const {
 	return length;
 }
 
-
-
+/**
+ * Function: printList
+ * Description: prints length of list as well as visual representation of list
+ * Parameters: none
+ * Return: void
+*/
 template<typename T>
-void DoublyLinkedList<T>::printList() const {
-	std::cout << "---------------------" << std::endl;
+void DoublyLinkedList<T>::printList() {
+	std::cout << std::endl;
 	if(isEmpty()) {
 		std::cout << "List is empty!" << std::endl;
 	} else {
-		Node *temp;
-		temp = head;
-		std::cout << "Length of list: " << getLength() << std::endl << "List = ";
-		while(temp != nullptr) {
-			std::cout << "[" << temp->data << "]";
-			if(temp->next != nullptr) {
-				std::cout << "-->";
+		std::cout << "Length of list: " << getLength() << std::endl << "List: [H] <--> ";
+		for(Iterator itr = begin(); itr != end(); ++itr) {
+			std::cout << "[" << *itr << "]";
+			if(itr.next() != end()) {
+				std::cout << " <--> ";
 			}
-			temp = temp->next;
 		}
-		std::cout << std::endl;
+		std::cout << " <--> [T]" << std::endl;
 	}
-	std::cout << "---------------------" << std::endl;
+	std::cout << std::endl;
 }
 
+/**
+ * Function: pushFront
+ * Description: inserts new node at front of list and increments length
+ * Parameters: const T val
+ * Return: void
+*/
 template<typename T>
 void DoublyLinkedList<T>::pushFront(const T val) {
 	Node *temp = new Node();
 	temp->data = val;
-	if(isEmpty()) {
-		temp->next = nullptr;
-		temp->prev = nullptr;
-		head = temp;
-		tail = temp;
-	} else {
-		temp->next = head;
-		head->prev = temp;
-		temp->prev = nullptr;
-		head = temp;	
-	}
+	temp->next = head->next;
+	temp->prev = head;
+	head->next->prev = temp;
+	head->next = temp;
 	length++;
 }
 
+/**
+ * Function: pushBack
+ * Description: inserts new node at end of list and increments length
+ * Parameters: const T val
+ * Return: void
+*/
 template<typename T>
 void DoublyLinkedList<T>::pushBack(const T val) {
 	Node *temp = new Node();
 	temp->data = val;
-	if(isEmpty()) {
-		temp->next = nullptr;
-		temp->prev = nullptr;
-		head = temp;
-		tail = temp;
-	} else {
-		temp->prev = tail;
-		tail->next = temp;
-		temp->next = nullptr;
-		tail = temp;
-	}
+	temp->prev = tail->prev;
+	temp->next = tail;
+	tail->prev->next = temp;
+	tail->prev = temp;
 	length++;
 }
 
+/**
+ * Function: popFront
+ * Description: removes node from front of list and decrements length, does not
+ *          	allow delete if list is empty
+ * Parameters: none
+ * Return: void
+*/
 template<typename T>
 void DoublyLinkedList<T>::popFront() {
 	if(isEmpty()) {
 		std::cout << "Err(popFront): Can't delete from empty list!" << std::endl;
 	} else {
-		Node *temp;
-		temp = head;
-		head = head->next;
+		Node *temp = head->next;
+		head->next = temp->next;
+		head->next->prev = head;
 		delete temp;
 		length--;
-		if(head != nullptr) {
-			head->prev = nullptr;
-		} else {
-			tail = head;
-		}
 	}
 }
 
+/**
+ * Function: popBack
+ * Description: removes node from back of list and decrements length, does not
+ *          	allow delete if list is empty
+ * Parameters: none
+ * Return: void
+*/
 template<typename T>
 void DoublyLinkedList<T>::popBack() {
 	if(isEmpty()) {
 		std::cout << "Err(popBack): Can't delete from empty list!" << std::endl;
 	} else {
-		Node *temp;
-		temp = tail;
-		tail = tail->prev;
+		Node *temp = tail->prev;
+		tail->prev = temp->prev;
+		tail->prev->next = tail;
 		delete temp;
 		length--;
-		if(tail != nullptr) {
-			tail->next = nullptr;
-		} else {
-			head = tail;
-		}
 	}
 }
 
+/**
+ * Function: reverse
+ * Description: reverses the order of the nodes in the list
+ * Parameters: none
+ * Return: void
+*/
 template<typename T>
 void DoublyLinkedList<T>::reverse() {
-	if(isEmpty() || getLength() == 1) {
-		std::cout << "Err(reverse): List is empty or contains a single node (no action required)" << std::endl;
-	} else {
-		Node *temp, *current;
-		current = head;
-		while(current != nullptr) {
-			temp = current->prev;
-			current->prev = current->next;
-			current->next = temp;
-			current = current->prev;
-		}
-		temp = head;
-		head = tail;
-		tail = temp;
+	Node *temp, *current;
+	current = head;
+	while(current != nullptr) {
+		temp = current->prev;
+		current->prev = current->next;
+		current->next = temp;
+		current = current->prev;
 	}
+	temp = head;
+	head = tail;
+	tail = temp;
+
 }
 
+/**
+ * Function: find
+ * Description: provides iterator pointing to the first node found that contains
+ *              val, otherwise provides iterator pointing to tail node
+ * Parameters: const T val
+ * Return: Iterator
+*/
+template<typename T>
+typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::find(const T val) {
+	for(Iterator itr = begin(); itr != end(); ++itr) {
+		if(*itr == val) {
+			return itr;
+		}
+	}
+	return end();
+}
+
+/**
+ * Function: begin
+ * Description: provides iterator pointing to node directly after head node
+ * Parameters: none
+ * Return: Iterator
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::begin() {
-	return Iterator(head);
+	return Iterator(head->next);
 }
 
+/**
+ * Function: end
+ * Description: provides iterator pointing to tail node
+ * Parameters: none
+ * Return: Iterator
+*/
 template<typename T>
 typename DoublyLinkedList<T>::Iterator DoublyLinkedList<T>::end() {
-	return Iterator(nullptr);
+	return Iterator(tail);
 }
 
+/**
+ * Function: front
+ * Description: gets value stored in node directly after head node
+ * Parameters: none
+ * Return: T
+*/
+template<typename T>
+T DoublyLinkedList<T>::front() {
+	return *begin();
+}
+
+/**
+ * Function: back
+ * Description: gets value stored in node directly before tail node
+ * Parameters: none
+ * Return: T
+*/
+template<typename T>
+T DoublyLinkedList<T>::back() {
+	return *(--end());
+}
 
 
 #endif /* DOUBLYLINKEDLIST_H */
